380506a38c7bef63c80568ef3baf49a2ebde58df
Rahul-Pala <rahul_pala@hotmail.com>
2026-01-11 07:44:50 +1100

    TypeOfExamples

--- NullValue.js
+++ NullValue.js
@@ -1,3 +1,7 @@
 let someData = null;
 console.log(someData); // Output: null
 console.log(typeof someData); // Output: object
+
+let age;
+console.log(age); // Output: undefined
+console.log(typeof age); // Output: undefined
---
+++ ObjectTypes.js
@@ -0,0 +1,12 @@
+let trainer = {
+  id: 'trainer',
+  name: 'Rahul',
+  age: 30,
+};
+console.log(trainer);
+console.log(trainer.name);
+
+//add new property
+trainer.experience = 5;
+console.log(trainer.experience);
+console.log(typeof trainer);
